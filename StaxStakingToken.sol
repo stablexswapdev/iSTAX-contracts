091617a54dd605bc9fcf777c72d2ea6189b3c371
stablexswapdev <73826792+stablexswapdev@users.noreply.github.com>
2020-12-17 13:42:37 +0800

    staxstaking

---
+++ StaxStakingToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "./ERC20.sol";
+import "./Ownable.sol";
+
+// This contract is used really only for superficial purposes so that it's easier to see which pool is which
+// Tokens are actually held in the StaxStaking.sol contract
+
+contract StaxStakingToken is ERC20, Ownable {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 supply
+    ) public ERC20(name, symbol) {
+        _mint(msg.sender, supply);
+
+    }
+
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
+}
