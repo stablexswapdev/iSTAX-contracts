360e5ca404abf3bbc289512ec87a156db82674bb
stablexswapdev <73826792+stablexswapdev@users.noreply.github.com>
2021-01-12 20:38:35 +0800

    updating tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,7 @@ const iStaxToken = artifacts.require("iStaxToken");
 const iStaxIssuer = artifacts.require("iStaxIssuer");
 const stakingToken = artifacts.require("stakingToken");
 const iStaxMarket = artifacts.require("iStaxMarket");
+const iStaxMarketToken = artifacts.require("iStaxMarketToken");
 const devAddress = "0x7323B13669028780c6450A620064E30654a5Be2c" // To Update when testing
 
 module.exports = async function(deployer) {
@@ -17,8 +18,6 @@ await deployer.deploy(iStaxIssuer, iStaxAddress, devAddress, 8, 1, 250000, 25000
 const iStaxChef = await iStaxIssuer.deployed();
 const iStaxChefAddress = iStaxChef.address;
 
-
-
 //   Sample: deploys three different durations of staking dummy tokens, and mints 1 wei to the owner
   await deployer.deploy(stakingToken, 'StableX Staking Token', 'STAX2W', numAsHex)
   const twoWeekStake = await stakingToken.deployed();
@@ -28,15 +27,40 @@ const iStaxChefAddress = iStaxChef.address;
   const oneyearStake = await stakingToken.deployed();
 
 // Already Deployed addresses input here
-const stakingDurationTokenAddress = twoWeekStake.address;
+const stakingTokenAddress1 = twoWeekStake.address;
+const stakingTokenAddress2 = oneMonthStake.address;
+const stakingTokenAddress3 = oneyearStake.address;
+
+// Deploy Insurance Dummy Tokens
+// stablecoin peg protection
+await deployer.deploy(iStaxMarketToken), 'iStax DAIUP Insurance', 'iSTAXDAIUP', numAsHex)
+const daiUpToken = await iStaxMarketToken.deployed();
+await deployer.deploy(iStaxMarketToken), 'iStax DAIDOWN Insurance', 'iSTAXDAIDOWN', numAsHex)
+const daiUpToken = await iStaxMarketToken.deployed();
+await deployer.deploy(iStaxMarketToken), 'iStax USDTUP Insurance', 'iSTAXUSDTUP', numAsHex)
+const daiUpToken = await iStaxMarketToken.deployed();
+await deployer.deploy(iStaxMarketToken), 'iStax USDTDOWN Insurance', 'iSTAXUSDTDOWN', numAsHex)
+const daiUpToken = await iStaxMarketToken.deployed();
+
+
 const staxAddress = '0x0Da6Ed8B13214Ff28e9Ca979Dd37439e8a88F6c4' // This is only for mainnet, may need to toggle for testnet
 
-  await deployer.deploy(iStaxMarket, iStaxChefAddress, staxAddress ,iStaxAddress , stakingDurationTokenAddress,'1903600', '2306800','0')
-  const chef1 = await iStaxMarket.deployed();
-  const stakingToken1 = await stakingToken.at(stakingDurationTokenAddress)
-  await stakingToken1.mint(chef1.address, '1')
-  await chef1.depositToChef('1')
+// Launch First pool
+const poolWeight = 300; // Adjust if necessary for different pool weight multipliers.
+  await iStaxChef.add(poolWeight, stakingTokenAddress1, True)
+// Deploy market/staking contract 
+  await deployer.deploy(iStaxMarket, iStaxChefAddress, staxAddress ,iStaxAddress , stakingTokenAddress1,'1903600', '2306800','0')
+  const sousChef1 = await iStaxMarket.deployed();
+// const stakingToken1 = await stakingToken.at(stakingTokenAddress1)
+// Deposit 1 wei from owner to pool to initiate pool for mining rewards - not necessary if the weight = 0;
+  await twoweekStake.mint(sousChef1.address, '1')
+  await sousChef1.depositToChef('1')
+
+
+  
 
 }
 
 
+
+// todo: open ended distribution
